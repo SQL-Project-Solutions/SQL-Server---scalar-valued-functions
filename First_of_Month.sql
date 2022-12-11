3a3808009b64af41a04cf9ce29157b615cf988fb
Martina Bohunicka-Charles <105567611+mBohunickaCharles@users.noreply.github.com>
2022-12-11 18:31:14 +0000

    Update First_of_Month.sql

--- First_of_Month.sql
+++ First_of_Month.sql
@@ -1,10 +1,33 @@
 -- Get first day of the month
 
-CREATE OR ALTER FUNCTION [dbo].[FirstOfMonth] (@date DateTime)
+-- solution 1:
+CREATE FUNCTION [dbo].[FirstOfMonth] (@date DateTime)
 RETURNS DateTime AS
 
 BEGIN
-DECLARE @FoM DateTime
+    DECLARE @FoM DateTime
 	SET @FoM = CONVERT(DATE, DATEADD(DAY, 1-DATEPART(DAY, @date), @date))
-	RETURN @FoM
+    RETURN @FoM
+END
+
+
+-- solution 2:
+CREATE FUNCTION [dbo].[FirstOfMonth] (@date DateTime )
+RETURNS DateTime
+
+BEGIN
+    RETURN CAST(CAST(YEAR(@date) AS VARCHAR(4))
+            	+ '/' 
+				+ CAST(MONTH(@date) AS VARCHAR(2)) 
+				+ '/01' 
+				AS DateTime)
+END
+
+
+-- solution 3:
+CREATE OR FUNCTION [dbo].[FirstOfMonth] (@date DateTime )
+RETURNS DateTime
+
+BEGIN
+    RETURN CAST(FLOOR(CAST(@date AS DECIMAL(12, 5))) - (DAY(@date) - 1) AS DateTime)
 END
